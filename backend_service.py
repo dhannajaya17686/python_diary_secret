637186d59d6cf787e5f2cfba34aac601dbf515a4
Dhananjaya_Mudalige <71549074+dhannajaya17686@users.noreply.github.com>
2023-09-28 00:58:10 +0530

    This is a test message

---
+++ backend_service.py
@@ -0,0 +1,43 @@
+class Diary_DB_Backend():
+    def __init__(self) -> None:
+        DB_PATH=""
+        DB_NAME=""
+        DB_TABLE_NAMES=["","",""]
+    
+    #Database Creation Area is here This will be the database creation ares
+    def DB_create_fn()->bool:
+        pass
+
+    def DB_check_fn()->bool:
+        pass
+
+    def DB_schema_create_fn()->bool:
+        pass
+
+    #Database Add new items or data section
+
+    def DB_add_new_diary_fn()->bool:
+        pass
+
+    def DB_add_new_journal_fn()->bool:
+        pass
+
+    def DB_add_new_user_fn()->bool:
+        pass
+
+    
+    #Database Read items from the databse 
+
+    def DB_password_acceser_fn()->str:
+        pass
+
+    def DB_User_statsRead_fn()->str:
+        pass
+
+    def DB_user_diaryItemsRead_fn()->str:
+        pass
+
+
+
+
+
